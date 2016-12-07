69a63c6b13bc1965fd54285c827d8db0db425103
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-12-07 12:31:52 +0300

    repseqio

--- repseqio.rb
+++ repseqio.rb
@@ -1,8 +1,8 @@
 class Repseqio < Formula
   homepage "https://github.com/repseqio/repseqio"
-  url "https://github.com/repseqio/repseqio/releases/download/v1.2.2/repseqio-1.2.2.zip"
-  version "1.2.2-1"
-  sha256 "13280a27d6e72d2259eaa24120ced48a42237cb37fcc11b356d98e9f1154330d"
+  url "https://github.com/repseqio/repseqio/releases/download/v1.2.3/repseqio-1.2.3.zip"
+  version "1.2.3-1"
+  sha256 "36882a10662de214c83dde6ff8edca96b2b4d3c05f3c7d56eab7cb819d96e59f"
 
   depends_on :java
 
